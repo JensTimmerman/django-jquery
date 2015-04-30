80519ac549c943d054a7a5a80c442b886fdfe545
ssash <a.e.saveyko@gmail.com>
2015-04-30 10:31:16 +0300

    Fix typo.

--- jquery/templatetags/jquery_tags.py
+++ jquery/templatetags/jquery_tags.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.forms.widgets import flatatt
+from django.forms.utils import flatatt
 from django.template import Library
 
 from jquery.utils import jquery_path
@@ -9,7 +9,7 @@ register = Library()
 
 @register.simple_tag
 def jquery_script():
-    return '<script{0}></script>'.format(flatattr({
+    return '<script{0}></script>'.format(flatatt({
         'type': 'text/javascript',
         'src': settings.MEDIA_URL + jquery_path,
     }))
